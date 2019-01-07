041eb61d69b3fb8a63fdbd23804f8c34bc18331a
lidiahandra <43603469+lidiahandra@users.noreply.github.com>
2019-01-07 08:56:08 +0700

    Add files via upload

---
+++ lidia.php
@@ -0,0 +1,50 @@
+<?php
+
+//pertemuan.1
+//standart output
+//echo,print,var_dump
+
+//echo "Hello Word";
+//var_dump("Hello Word")
+
+//penuisan php
+//1.html dalam php
+//2.php dalam html
+
+//$variable
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>praktikum</title>
+</head>
+<body>
+	<h1>TUGAS MATKUL BAPER 3</h1>
+
+	<h4><?php echo "Nama Lengkap			: Lidia Anugerah Heni Gelang" ?></h1>
+	<h4><?php echo "NPM  					: 170403010067" ?></h1>
+	<h4><?php echo "Alamat Sekarang  		: Jl.Keben 2 Blok B" ?></h1>
+	<h4><?php echo "No. HP  				: 082143434824" ?></h1>
+	<h4><?php echo "Jurusan  				: TEKNIK INFORMATIKA" ?></h1>
+	<h4><?php echo "Fakultas 				: Sains Dan Teknologi" ?></h1>
+	<h4><?php echo "Hobby  					: Volly" ?></h1>
+	<h4><?php echo "Cita - Cita  			: Atlet Volly" ?></h1>
+	<h4><?php echo "Moto Hidup  			: Terserah " ?></h1>
+</body>
+</html>
+<?php
+
+	echo "<br><br><h1>TUGAS MATKUL BAPER 3";
+
+	echo "<h4>Nama Lengap 				: Lidia Anugerah Heni Gelang </h1>";
+	echo "<h4>NPM 						: 170403010067</h1>";
+	echo "<h4>Alamat Sekarang 			: Jl.Keben 2 Blok B</h1>";
+	echo "<h4>No. HP 					:082143434824 </h1>";
+	echo "<h4>Jurusan 					: Teknik Informatika</h1>";
+	echo "<h4>Fakultas 					: Sains Dan Teknologi</h1>";
+	echo "<h4>Hoby 						:Volly</h1>";
+	echo "<h4>Cita - Cita 				:Atlet Volly</h1>";
+	echo "<h4>Moto Hidup 				:Terserah</h1>";
+
+	?>
